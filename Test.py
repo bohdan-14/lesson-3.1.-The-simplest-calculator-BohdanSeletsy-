a27aec12ae91523545a7a781f1c059006ba817f8
Bohdan <bohdan.sell23@gmail.com>
2024-08-22 13:00:11 +0300

    Initial commit

---
+++ Test.py
@@ -0,0 +1,18 @@
+n1 = int(input("Enter the  first number: "))
+action = input ("Enter the action (+,-,*,/):")
+n2 = int(input("Enter the  second number: "))
+if action == "+":
+    result = n1 + n2
+elif action == "-":
+    result = n1 - n2
+elif action == "*":
+    result = n1 * n2
+elif action == "/":
+    if n2 != 0:
+        result = n1 / n2
+    else:
+        print ("Помилка: Ділення на 0 неможливе!")
+else:
+    print ("Невідома дія!")
+print ("RESULT:", result)
+
