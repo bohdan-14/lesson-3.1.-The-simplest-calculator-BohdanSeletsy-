6051b2bd54a4634a2bd5caf59d195d77c1fb7609
Bohdan <bohdan.sell23@gmail.com>
2024-08-22 17:55:24 +0300

    lesson 3.3 Split one list into two lists (BohdanSeletsy)

--- test 3.py
+++ test 3.py
@@ -5,3 +5,10 @@ ml1 = my_list[:mid]
 ml2 = my_list[mid:]
 result = [ml1, ml2]
 print ("Result:", result)
+my_list = input("Enter a list:")
+Len = len(my_list)
+mid = (Len+1)//2
+ml1 = my_list[:mid]
+ml2 = my_list[mid:]
+result = [ml1, ml2]
+print ("Result:", result)
