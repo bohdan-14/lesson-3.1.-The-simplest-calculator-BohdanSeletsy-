4c938c3391f30cc003a86b96efc69cc01a9819d5
bohdan-14 <bohdan.sell23@gmail.com>
2024-08-22 14:01:59 +0300

    Test.py

--- Test.py
+++ Test.py
@@ -1,9 +1,19 @@
-lst = [1, 2, 3, 4, 5]
-last_element = lst [-1]
-lst.pop (-1)
-lst.insert(-1, last_element)
-
-print(lst)
-
+n1 = int(input("Enter the  first number: "))
+action = input ("Enter the action (+,-,*,/):")
+n2 = int(input("Enter the  second number: "))
+if action == "+":
+    result = n1 + n2
+elif action == "-":
+    result = n1 - n2
+elif action == "*":
+    result = n1 * n2
+elif action == "/":
+    if n2 != 0:
+        result = n1 / n2
+    else:
+        print ("Помилка: Ділення на 0 неможливе!")
+else:
+    print ("Невідома дія!")
+print ("RESULT:", result)
 
 
